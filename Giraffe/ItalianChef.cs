11a9c1db0e2c460195ce125c14b96cc457688cd5
Jan Thomas <thomas.jan.github@gmail.com>
2024-02-15 17:26:13 +0100

    first commit

---
+++ Giraffe/ItalianChef.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Giraffe
+{
+    internal class ItalianChef : Chef
+    {
+        public override void MakeSpecialDish()
+        {
+            Console.WriteLine("The Chef makes lasagne");
+        }
+        public void MakePasta()
+        {
+            Console.WriteLine("The Chef makes pasta");
+        }
+    }
+}
---
+++ Giraffe/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Giraffe
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Chef chef = new Chef();
+            chef.MakeSpecialDish();
+            
+            ItalianChef italianChef = new ItalianChef();
+            italianChef.MakeSpecialDish();
+   
+            Console.ReadLine();
+        }
+    }
+}
